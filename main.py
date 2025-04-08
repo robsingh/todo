c67a0c7098320326e2baf0e858bf48cf536454ea
robsingh <robie10895@gmail.com>
2025-04-07 20:57:44 -0400

    fixing a function

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+from datetime import date
+from datetime import datetime
+
 task_list = []
+
 def add_task():
     task_name = input("Enter the task details: ")
     task_list.append(task_name)
-    print(f"Task '{task_name}' added to the list!")
+    now = datetime.now()
+    date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
+    today_day = now.strftime("%A")
+    print(f"Task '{task_name}' added to the list on '{today_day}' at {date_time}.")
+
 
 def list_task():
       if not task_list:
@@ -17,28 +25,31 @@ def delete_task():
     list_task()
     try:
         taskToDelete = int(input("Enter the task # to delete: "))
-        if taskToDelete >= 0 and taskToDelete < len(task_list):
-            task_list.pop(taskToDelete)
-            print(f"Task {taskToDelete} has been removed")
+        index = taskToDelete - 1 # because user sees tasks starting from 1
+        if 0 <= index < len(task_list):
+            removed_task = task_list.pop(index)
+            print(f"Task '{removed_task}' has been removed.")
         else:
-            print(f"Task #{taskToDelete} was not found! Please try again with correct input")
-    except:
-        print("Invalid Input!")
+            print(f"Task #{taskToDelete} was not found! Please try again with correct input.")
+    except ValueError:
+        print("Invalid Input! Please enter a number.")
     
-
-
 
 
 if __name__ == "__main__":
         print("Welcome to the To-Do list app, a very introductory app!")
         while True:
             print("\n")
-            print("Please select one of the following options")
-            print("\n")
+            print("Please select one of the following options:")
+            print()
             print("1. Add a new Task")
+            print()
             print("2. Delete a Task")
+            print()
             print("3. List Tasks")
+            print()
             print("4. Quit!")
+            print()
 
             choice = input("Enter your choice: ") # use int(input()) later
 
