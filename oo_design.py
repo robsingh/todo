ad3bd4c8de50be829dcd9a388049a6f5f86da5bc
robsingh <robie10895@gmail.com>
2025-04-18 22:16:35 -0400

    OO Design

--- oo_design.py
+++ oo_design.py
@@ -29,6 +29,7 @@ class TaskManager:
         self.deleted_tasks = []
         self.setup_database() #ensures DB is ready when object is created
 
+
     def setup_database(self):
         '''
         creates the task table if it does not exist
@@ -74,13 +75,8 @@ class TaskManager:
             print(f"Failed to connect with the database: {e}")
 
 
-
     def load_from_db(self):
-        '''
-        load active tasks from db
-        convert each record into a Task object
-        append them to self.tasks
-        '''
+        self.tasks.clear()
         try:
             with sqlite3.connect('todo.db') as conn:
                 cursor = conn.cursor()
@@ -101,17 +97,109 @@ class TaskManager:
             
         except sqlite3.OperationalError as e:
             print(f"Database Error: {e}")
+            
 
     def list_tasks(self):
-        pass
+        if not self.tasks:
+            print(f"No tasks loaded in memory.")
+            return
+        
+        print(f"{'ID':<4} {'Task':<30} {'Priority':<8} {'Status':<10} {'Created At'}")
+        print("-" * 70)
+        for task in self.tasks:
+            print(task)
+        
     
     def mark_task_completed(self, id):
-        pass
+        try:
+            with sqlite3.connect('todo.db') as conn:
+                cursor = conn.cursor()
+                cursor.execute('''
+                UPDATE tasks
+                SET completed = 1
+                where id = ? AND removed_at IS NULL
+                ''', (id,))
+                conn.commit()
+                
+                if cursor.rowcount == 0:
+                    print(f"❌ No active tasks found with {id}.")
+                else:
+                    print(f"✅ {id} mark as completed.")
+                
+                for task in self.tasks:
+                    if task.id == id:
+                        task.mark_completed()
+                        print(f"✅ Task marked as completed.")
+                        print(task)
+
+        except ValueError:
+            print("Invalid Input! Please enter a valid task ID.")
+        except sqlite3.OperationalError as e:
+            print(f"Database Error: {e}")
 
     def delete_task(self, id):
-        pass
+        task_to_delete = None
+        for task in self.tasks:
+            if task.id == id:
+                task.mark_deleted()
+                task_to_delete = task
+                break
+
+        if not task_to_delete:
+            print(f"❌ Task with ID {id} not found in memory.")
+            return
+        
+        try:
+            with sqlite3.connect('todo.db') as conn:
+                cursor = conn.cursor()
+                cursor.execute('''
+                               UPDATE tasks
+                               SET removed_at = ?
+                               WHERE id = ? AND removed_at IS NULL
+                ''', (task_to_delete.removed_at, id))
+                conn.commit()
+
+                if cursor.rowcount == 0:
+                    print(f"❌ No active tasks found with ID {id}.")
+                else:
+                    print(f"Task {id} is deleted.")
+
+                    self.tasks.remove(task_to_delete)
+                    self.deleted_tasks.append(task_to_delete)
+
+        except ValueError:
+            print("Invalid Input! Please enter a valid task ID.")
+        except sqlite3.OperationalError as e:
+            print(f"Database Error: {e}")
+
 
     def view_deleted_tasks(self):
-        pass
+        try:
+            with sqlite3.connect('todo.db') as conn:
+                cursor = conn.cursor()
+                cursor.execute('''
+                        SELECT id, name, created_at, priority, completed, removed_at
+                        FROM tasks 
+                        WHERE removed_at IS NOT NULL
+                        ORDER BY removed_at DESC
+                        ''')
+                deleted = cursor.fetchall()
+
+                if not deleted:
+                    print(f"No deleted tasks found!")
+                    return
+                
+                print(f"{'ID':<4} {'Task':<30} {'Priority':<8} {'Status':<10} {'Created At':<20} {'Removed At'}")
+                print("-" * 100)
+                for row in deleted:
+                    task_id, name, created_at, priority, completed, removed_at = row
+                    task = Task(task_id, name, created_at, priority, completed, removed_at)
+                    print(task)
+                    self.deleted_tasks.append(task)
+
+        except sqlite3.OperationalError as e:
+            print(f"Database Error: {e}")
+
+                
 
     
